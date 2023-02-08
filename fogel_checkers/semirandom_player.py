ccce7ef668ff7207802490b1ccfa1a4d13d5ce9d
JHpusd <justinhongdebate@gmail.com>
2023-02-08 20:57:50 +0000

    optimized semirandom player

--- fogel_checkers/semirandom_player.py
+++ fogel_checkers/semirandom_player.py
@@ -8,13 +8,7 @@ class SemirandomPlayer():
         self.player_num = player_num
     
     def choose_move(self, board, options):
-        all_translations = [move[1] for move in options]
-        capt_idxs = []
-        for i, translation in enumerate(all_translations):
-            if abs(translation[0]) == 2:
-                capt_idxs.append(i)
-        if len(capt_idxs) == 0:
+        captures = [move for move in options if abs(move[1][0]) == 2]
+        if len(captures) == 0:
             return random.choice(options)
-        else:
-            rand_idx = random.choice(capt_idxs)
-            return options[rand_idx]
+        return random.choice(captures)
